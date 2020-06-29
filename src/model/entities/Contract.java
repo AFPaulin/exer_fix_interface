b0e9569fc2d1a57097c2eac73dc1294101300fb6
Andre Faramilio Paulin <andrefaramilio121@gmail.com>
2020-06-29 17:07:58 -0300

    Exercicio interface nova composição:serviços

---
+++ src/model/entities/Contract.java
@@ -0,0 +1,71 @@
+package model.entities;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Contract {
+
+	private Integer number;
+	private Date date;
+	private Double totalValue;
+	
+	/* Variavel é instanciada assim quando ela possui varios itens */
+	private List<Installment> installments = new ArrayList<>();
+
+	/* Construtores */
+	public Contract () {
+	}
+	
+	public Contract(Integer number, Date date, Double totalValue) {
+		this.number = number;
+		this.date = date;
+		this.totalValue = totalValue;
+	}
+
+	/* Getters e Setters */
+	
+	public Integer getNumber() {
+		return number;
+	}
+
+	public void setNumber(Integer number) {
+		this.number = number;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public Double getTotalValue() {
+		return totalValue;
+	}
+
+	public void setTotalValue(Double totalValue) {
+		this.totalValue = totalValue;
+	}
+	
+	
+	/*Construtor da lista deve ser alterado pela funcao 
+	 * add e remove não através de getter e setter */
+	 
+	
+	public List<Installment> getInstallments() {
+		return installments;
+	}
+	
+	public void addInstallment(Installment installment) {
+		installments.add(installment);
+	}
+	
+	public void removeInstallment(Installment installment) {
+		installments.remove(installment);
+	}
+	
+	
+	
+}
---
+++ src/module-info.java
@@ -0,0 +1,3 @@
+module exer_fix_interface {
+	requires java.xml;
+}
